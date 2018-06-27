76bf236150e73d6ab5a7bc4a5a65e3c0e9e77458
Arthur Jen <arthurljen@gmail.com>
2018-06-27 09:51:18 -0700

    delete passes

--- test/database.test.js
+++ test/database.test.js
@@ -1,17 +1,15 @@
 const assert = require('assert');
 const path = require('path');
 const Store = require('../lib/Store');
-const { rimraf, mkdirp, readdir, readFile } = require('../lib/fs');
+const { rimraf, mkdirp } = require('../lib/fs');
 
 describe('store', () => {
 
-    let store = null;
     const dest = path.join(__dirname, 'brain');
+    let store = new Store(dest);
 
     beforeEach(() => {
-        store = new Store(dest);
         return rimraf(dest);
-
     });
 
     beforeEach(() => {
@@ -19,14 +17,31 @@ describe('store', () => {
     });
 
     it('saves a file to brain with _id property', () => {
-        return store.save({ thought: 'I\'m so hungry!' })
+        return store.save({ thought: 'I miss Chris.' })
             .then(saved => {
-                assert.ok(JSON.parse(saved)._id);
+                return store.get(saved._id);
+            })
+            .then(got => {
+                assert.equal(got.thought, 'I miss Chris.');
             });
     });
 
-    it('gets a file based on _id', () => {
+    it('returns null if file does not exist', () => {
         return store.get('badId')
+            .then(got => {
+                assert.equal(got, null);
+            });
+    });
+
+    it('deletes a file by id', () => {
+        return store.save({ thought: 'ugh allergies' })
+            .then(saved => {
+                return store.delete(saved._id);
+            })
+            .then(response => {
+                assert.equal(response.removed, true);
+                return store.get(response._id);
+            })
             .then(got => {
                 assert.equal(got, null);
             });
