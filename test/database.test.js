bd0e05bc28cf08844882752baa5d21d616af1b8e
Arthur Jen <arthurljen@gmail.com>
2018-06-27 07:48:58 -0700

    save test now confirms _id property

--- test/database.test.js
+++ test/database.test.js
@@ -1,12 +1,12 @@
 const assert = require('assert');
 const path = require('path');
-const Store = require('../lib/database');
-const { rimraf, mkdirp } = require('../lib/fs');
+const Store = require('../lib/Store');
+const { rimraf, mkdirp, readdir, readFile } = require('../lib/fs');
 
 describe('store', () => {
 
     let store = null;
-    const dest = path.join(__dirname, 'save-dir');
+    const dest = path.join(__dirname, 'brain');
 
     beforeEach(() => {
         store = new Store(dest);
@@ -18,10 +18,19 @@ describe('store', () => {
         return mkdirp(dest);
     });
 
-    it('saves a file', () => {
-        return store.save({
-            thought: 'I\'m so hungry!'
-        });
-        // has _id property
+    it.only('saves a file to brain with _id property', () => {
+        return store.save({ thought: 'I\'m so hungry!' })
+            .then(() => {
+                return readdir(dest);
+            })
+            .then(contents => {
+                const savedFileName = path.join(dest, contents[0]);
+                return readFile(savedFileName, 'utf8');
+            })
+            .then(saved => {
+                const test = JSON.parse(saved);
+                assert.ok(test._id);
+            });
     });
+
 });
