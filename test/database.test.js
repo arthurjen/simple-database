44b10c2c474387a1821d614e2a55c8387d063965
Arthur Jen <arthurljen@gmail.com>
2018-06-26 17:44:48 -0700

    saving with an id property works

--- test/database.test.js
+++ test/database.test.js
@@ -1,4 +1,27 @@
 const assert = require('assert');
 const path = require('path');
-const fs = require('../lib/fs');
 const Store = require('../lib/database');
+const { rimraf, mkdirp } = require('../lib/fs');
+
+describe('store', () => {
+
+    let store = null;
+    const dest = path.join(__dirname, 'save-dir');
+
+    beforeEach(() => {
+        store = new Store(dest);
+        return rimraf(dest);
+
+    });
+
+    beforeEach(() => {
+        return mkdirp(dest);
+    });
+
+    it('saves a file', () => {
+        return store.save({
+            thought: 'I\'m so hungry!'
+        });
+        // has _id property
+    });
+});
